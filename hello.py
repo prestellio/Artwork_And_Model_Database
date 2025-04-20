8891da254af7c5bb36c3e3d98efc7cfc5252cc8c
Preston Elliott <149803791+prestellio@users.noreply.github.com>
2025-04-19 23:55:08 -0500

    Created Delete function for Application

---
+++ hello.py
@@ -0,0 +1,84 @@
+from flask import Flask, jsonify, render_template, request, redirect, url_for
+from crud import *
+import sqlite3
+
+# flask --app hello.py run
+# to start application temp server
+
+# Sci-fi and Fantasy app - Flask connection
+sfapp = Flask(__name__, static_folder='static')
+
+# When the user navigates to '/' ...
+@sfapp.route("/")
+def HomePageRender():   # Renders the main HTML page
+    return render_template('index.html')
+
+
+# Executes the provided file (usually my sql files)
+def ExecuteSQLFile(fileName):
+    with open(fileName, 'r') as file:
+        sqlInstruc = file.read()
+    
+    conn = sqlite3.connect('database.db') # Navigate the database
+    cursor = conn.cursor()
+
+    cursor.executescript(sqlInstruc)  # Execute instructions
+    conn.commit()
+    conn.close()
+
+# Create tables and insert data into db
+if __name__ == '__main__':
+    # Only execute if tables don't already exist
+    try:
+        ExecuteSQLFile('create.sql')
+        ExecuteSQLFile('insert.sql')
+    except Exception as e:
+        print("Error executing SQL files:", e)
+
+    sfapp.run(debug=True)
+
+
+# Runs query to database
+@sfapp.route('/prices')
+def GetPrices():
+    pricesNames, pricesData = RunQuery("SELECT * FROM prices;")
+
+    prices = {
+        "headers": pricesNames,
+        "data": pricesData
+    }
+
+    return jsonify(prices)  # Display data as JSON
+
+@sfapp.route('/insert', methods=['POST'])
+def InsertValues():
+    #table = request.form.get('form_name')
+    values = (request.form.get('input1'), request.form.get('input2'), request.form.get('input3'), request.form.get('input4'), request.form.get('input5'))
+
+    InsertData(values)
+
+    #GetPrices()
+    return redirect(url_for('HomePageRender'))
+
+
+@sfapp.route('/delete', methods=['POST'])
+def DeleteRow():
+    idvalue = request.form.get('delete')
+
+    DeleteData(idvalue)
+
+    return redirect(url_for('HomePageRender'))
+
+
+
+if __name__ == '__main__':
+    sfapp.run(debug=True)
+
+
+
+
+
+
+
+
+
